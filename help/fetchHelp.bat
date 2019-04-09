cddc09998bfe90a82b0a664bb7b8a41a117186f9
xavierdpt@gmail.com <xavierdpt@gmail.com>
2019-04-09 17:00:14 +0200

    .

---
+++ help/fetchHelp.bat
@@ -0,0 +1,115 @@
+oc help
+oc options
+oc help types
+oc help login
+oc help new-project
+oc help new-app
+oc help status
+oc help project
+oc help projects
+oc help explain
+oc help cluster
+oc help rollout
+oc help rollback
+oc help new-build
+oc help start-build
+oc help cancel-build
+oc help import-image
+oc help tag
+oc help get
+oc help describe
+oc help edit
+oc help set
+oc set env --help
+oc set resources --help
+oc set volumes --help
+oc set probe --help
+oc set deployment-hook --help
+oc set image --help
+oc set build-secret --help
+oc set image-lookup --help
+oc set triggers --help
+oc set build-hook --help
+oc set route-backends --help
+oc help label
+oc help annotate
+oc help expose
+oc help delete
+oc help scale
+oc help autoscale
+oc help secrets
+oc help serviceaccounts
+oc help logs
+oc help rsh
+oc help rsync
+oc help port-forward
+oc help debug
+oc help exec
+oc help proxy
+oc help attach
+oc help run
+oc help cp
+oc help wait
+oc help adm
+oc adm options
+oc adm router --help
+oc adm ipfailover --help
+oc adm registry --help
+oc adm new-project --help
+oc adm policy --help
+oc adm groups --help
+oc adm ca --help
+oc adm certificate --help
+oc adm create-node-config --help
+oc adm manage-node --help
+oc adm cordon --help
+oc adm uncordon --help
+oc adm drain --help
+oc adm taint --help
+oc adm pod-network --help
+oc adm diagnostics --help
+oc adm prune --help
+oc adm build-chain --help
+oc adm migrate --help
+oc adm top --help
+oc adm verify-image-signature --help
+oc adm create-kubeconfig --help
+oc adm create-api-client-config --help
+oc adm create-bootstrap-project-template --help
+oc adm create-boostrap-policy-file --help
+oc adm create-login-template --help
+oc adm create-provider-selection-template --help
+oc adm create-error-template --help
+oc adm completion --help
+oc adm config --help
+oc help create
+oc help replace
+oc help apply
+oc help patch
+oc help process
+oc help export
+oc help extract
+oc help idle
+oc help observe
+oc help policy
+oc help auth
+oc help convert
+oc help import
+oc help image
+oc image append --help
+oc image extract --help
+oc image mirror --help
+oc help registry
+oc registry info --help
+oc registry login --help
+oc help api-versions
+oc help api-resources
+oc help logout
+oc help config
+oc help whoami
+oc help completion
+oc help ex
+oc help help
+oc help plugin
+oc help version
+oc api-resources
